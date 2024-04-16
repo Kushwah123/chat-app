51773c3010c3592b81e3cde103c1edf102dd5291
DHARMENDRA KUMAR <dharmendrakumar1208@gmail.com>
2024-04-16 17:46:05 +0530

    hello

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -88,10 +88,6 @@ export default function Register() {
     <>
       <FormContainer>
         <form action="" onSubmit={(event) => handleSubmit(event)}>
-          <div className="brand">
-            <img src={Logo} alt="logo" />
-            <h1>snappy</h1>
-          </div>
           <input
             type="text"
             placeholder="Username"
